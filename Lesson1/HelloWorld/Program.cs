08994e7f1708a69be68f5453c59c988b180ba51e
svfleytin <fleytin@gmail.comfleytin@hotmail.com>
2021-01-19 12:43:43 +0300

    Created my first app.

---
+++ Lesson1/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, how can I call you?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Today is {DateTime.Now}.");
+            Console.WriteLine($"Hello, {name}! Today is {DateTime.Now}.");
+        }
+    }
+}
